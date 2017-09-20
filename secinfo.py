ff07ed4f361dcc9c412ef0025ac97b2f6aeab67f
Ilya Kuydich <agree2012@yandex.ru>
2017-09-20 11:23:13 +0300

    Update secinfo.py

--- secinfo.py
+++ secinfo.py
@@ -1,4 +1,4 @@
-API_Token='xoxb-238149745300-5ixYBUIAGmBlqcNvK7vUf0WT'
+API_Token='xoxb-238149745300-jurJRcfXoDpwq2cBxOjOqDWm'
 username='m.korostelev'
 passowrd='654123Misha'
 channel_id = 'D715E0CB1'
