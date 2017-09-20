a092f7b5dfe50961eaac2a05b0045fde147445af
Ilya Kuydich <agree2012@yandex.ru>
2017-09-20 12:36:45 +0300

    Delete secinfo.py

--- secinfo.py
+++
@@ -1,6 +0,0 @@
-API_Token='xoxb-238149745300-ihW5qHRsx3YfE9PgaesdBu29'
-username='m.korostelev'
-passowrd='654123Misha'
-channel_id = 'D715E0CB1'
-userId = ['U6ZGR7X5X','U6P9XLGAD','U6ZCSAGQ0']
-userName = ['Mikhail', 'Aleksei', 'Ilya']
