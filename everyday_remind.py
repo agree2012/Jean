8ff9330cb8d4468a9454c791bbc3ef5048d29315
Ilya Kuydich <agree2012@yandex.ru>
2017-10-02 14:24:25 +0300

    Update everyday_remind.py

--- everyday_remind.py
+++ everyday_remind.py
@@ -38,8 +38,8 @@ def every_remind(i):
     day = ''
     if 'every' in days_event:
         if 'day' in days_event:
-            schedule.every().day.at(str_time(i)).do(remind_function.remind_event_and_name, i)
+            schedule.every().day.at(str_time(i)).do(remind_function.event_sched_print, i)
         else:
             for j in range(0,7):
                 if day_of_week[j] in days_event:
-                    every_day_of_week(j,str_time(i),remind_function.remind_event_and_name,i)
+                    every_day_of_week(j,str_time(i),remind_function.event_sched_print,i)
