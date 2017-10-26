94bb3447fe8d78582b387595bd4be740adb695c0
MihKorostelev <mihaill170996@yahoo.com>
2017-10-26 12:05:49 +0300

    Update jean_ask.py

--- jean_ask.py
+++ jean_ask.py
@@ -158,7 +158,7 @@ def threading_start():
 
 
 def ask_about_work():
-    schedule.every().day.at("12:05").do(threading_start)
+    schedule.every().day.at("12:15").do(threading_start)
 
 
 def clear_dolist():
